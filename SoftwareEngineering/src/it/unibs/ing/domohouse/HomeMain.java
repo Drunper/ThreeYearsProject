292aafd663bf21823428a4162bea322e200b4115
Federico <Federico@192.168.1.13>
2020-02-29 14:45:33 +0100

    Initializing Version 3

--- SoftwareEngineering/src/it/unibs/ing/domohouse/HomeMain.java
+++ SoftwareEngineering/src/it/unibs/ing/domohouse/HomeMain.java
@@ -6,6 +6,7 @@ public class HomeMain {
 	
 	private static InterfaceHandler interfaceHandler;
 	
+	//version 3
 	public static void main(String[] args) {
 		interfaceHandler = new InterfaceHandler();
 		interfaceHandler.show();
